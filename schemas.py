0fb8c47d5b912005d7e876eb4f1cea97f433dba0
Andrei Meshchenenkov <meschenenkovjob@gmail.com>
2025-07-06 09:15:34 +0400

    homework done

--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
-post_users = {
+#схемагенератор https://www.liquid-technologies.com/online-json-to-schema-converter
+post_users = { #изУрока
     "$schema": "http://json-schema.org/draft-04/schema#",
     "type": "object",
     "additionalProperties": False,
@@ -24,29 +25,21 @@ post_users = {
     ]
 }
 
-#1
-put_update = {
+
+post_register_unsuccessful = {
   "$schema": "http://json-schema.org/draft-04/schema#",
   "type": "object",
   "properties": {
-    "name": {
-      "type": "string"
-    },
-    "job": {
-      "type": "string"
-    },
-    "updatedAt": {
+    "error": {
       "type": "string"
     }
   },
   "required": [
-    "name",
-    "job",
-    "updatedAt"
+    "error"
   ]
 }
 
-#2
+
 post_create = {
   "$schema": "http://json-schema.org/draft-04/schema#",
   "type": "object",
@@ -72,280 +65,8 @@ post_create = {
   ]
 }
 
-get_users = {
+
+get_single_user_not_found = {
   "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "properties": {
-    "page": {
-      "type": "integer"
-    },
-    "per_page": {
-      "type": "integer"
-    },
-    "total": {
-      "type": "integer"
-    },
-    "total_pages": {
-      "type": "integer"
-    },
-    "data": {
-      "type": "array",
-      "items": [
-        {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "integer"
-            },
-            "email": {
-              "type": "string"
-            },
-            "first_name": {
-              "type": "string"
-            },
-            "last_name": {
-              "type": "string"
-            },
-            "avatar": {
-              "type": "string"
-            }
-          },
-          "required": [
-            "id",
-            "email",
-            "first_name",
-            "last_name",
-            "avatar"
-          ]
-        },
-        {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "integer"
-            },
-            "email": {
-              "type": "string"
-            },
-            "first_name": {
-              "type": "string"
-            },
-            "last_name": {
-              "type": "string"
-            },
-            "avatar": {
-              "type": "string"
-            }
-          },
-          "required": [
-            "id",
-            "email",
-            "first_name",
-            "last_name",
-            "avatar"
-          ]
-        },
-        {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "integer"
-            },
-            "email": {
-              "type": "string"
-            },
-            "first_name": {
-              "type": "string"
-            },
-            "last_name": {
-              "type": "string"
-            },
-            "avatar": {
-              "type": "string"
-            }
-          },
-          "required": [
-            "id",
-            "email",
-            "first_name",
-            "last_name",
-            "avatar"
-          ]
-        },
-        {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "integer"
-            },
-            "email": {
-              "type": "string"
-            },
-            "first_name": {
-              "type": "string"
-            },
-            "last_name": {
-              "type": "string"
-            },
-            "avatar": {
-              "type": "string"
-            }
-          },
-          "required": [
-            "id",
-            "email",
-            "first_name",
-            "last_name",
-            "avatar"
-          ]
-        },
-        {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "integer"
-            },
-            "email": {
-              "type": "string"
-            },
-            "first_name": {
-              "type": "string"
-            },
-            "last_name": {
-              "type": "string"
-            },
-            "avatar": {
-              "type": "string"
-            }
-          },
-          "required": [
-            "id",
-            "email",
-            "first_name",
-            "last_name",
-            "avatar"
-          ]
-        },
-        {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "integer"
-            },
-            "email": {
-              "type": "string"
-            },
-            "first_name": {
-              "type": "string"
-            },
-            "last_name": {
-              "type": "string"
-            },
-            "avatar": {
-              "type": "string"
-            }
-          },
-          "required": [
-            "id",
-            "email",
-            "first_name",
-            "last_name",
-            "avatar"
-          ]
-        }
-      ]
-    },
-    "support": {
-      "type": "object",
-      "properties": {
-        "url": {
-          "type": "string"
-        },
-        "text": {
-          "type": "string"
-        }
-      },
-      "required": [
-        "url",
-        "text"
-      ]
-    }
-  },
-  "required": [
-    "page",
-    "per_page",
-    "total",
-    "total_pages",
-    "data",
-    "support"
-  ]
-}
-
-get_single_user = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "properties": {
-    "data": {
-      "type": "object",
-      "properties": {
-        "id": {
-          "type": "integer"
-        },
-        "email": {
-          "type": "string"
-        },
-        "first_name": {
-          "type": "string"
-        },
-        "last_name": {
-          "type": "string"
-        },
-        "avatar": {
-          "type": "string"
-        }
-      },
-      "required": [
-        "id",
-        "email",
-        "first_name",
-        "last_name",
-        "avatar"
-      ]
-    },
-    "support": {
-      "type": "object",
-      "properties": {
-        "url": {
-          "type": "string"
-        },
-        "text": {
-          "type": "string"
-        }
-      },
-      "required": [
-        "url",
-        "text"
-      ]
-    }
-  },
-  "required": [
-    "data",
-    "support"
-  ]
-}
-
-
-
-
-post_register_unsuccessful = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "properties": {
-    "error": {
-      "type": "string"
-    }
-  },
-  "required": [
-    "error"
-  ]
+  "type": "object"
 }
